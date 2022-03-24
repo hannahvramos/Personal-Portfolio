cb27cb8cdf64a1bb4599b49a2a56a212cc2346ba
hannahvramos <hannahvictoriar@gmail.com>
2022-03-24 11:16:58 -0600

    adding starships

--- project folder/star wars/starships/sw-starships.js
+++ project folder/star wars/starships/sw-starships.js
@@ -1,15 +1,43 @@
 import { starships } from '../data/starships.js'
+import { removeChildren } from '../utils/index.js'
 
 const nav = document.querySelector('.nav')
 const navList = document.querySelector('.navList')
 const shipViewer = document.querySelector('.shipViewer')
+const modal = document.querySelector('.modal')
+const closeButton = document.querySelector('.modal-close')
+const shipMessage = document.querySelector('.shipMessage') //fix the class
+
+closeButton.addEventListener('click', ()=> {
+    modal.classList.toggle('is-active')
+})
 
 function populateNav() {
+    starships.forEach((starship) => {
+        let anchor = document.createElement('a')
+        anchor.href = ('#')
+        anchor.textContent = starship.name
+        anchor.addEventListener('click', () => populateShipView(starship))
 
+        const listItem = document.createElement('li') //the ul in html
+        listItem.appendChild(anchor)
+        navList.appendChild(listItem)
+    })
 }
+populateNav()
 
-function populateShipViewer() {
-
+function populateShipView(shipData) {
+    removeChildren(shipViewer)
+    const shipImage = document.createElement('img')
+    let shipNum = getLastNumber(shipData.url)
+    shipImage.src = `https://starwars-visualguide.com/assets/img/starships/${shipNum}.jpg`
+    shipImage.addEventListener('error', () => {
+        console.log('Error, Image not avaliable')
+        shipImage.hidden = true
+        modal.classList.toggle('is-active')
+        shipMessage.textContent = `The ship "${shipData.name}" has been lost in space`
+    })
+    shipViewer.appendChild(shipImage)
 }
 
 //for (let i = 0; i < starships.length; i++) { 
