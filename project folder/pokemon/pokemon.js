e813740863751e0e81ab528d17263652abf16381
hannahvramos <hannahvictoriar@gmail.com>
2022-04-05 11:16:17 -0600

    adding pokemon project

---
+++ project folder/pokemon/pokemon.js
@@ -0,0 +1,16 @@
+
+function getAPIData(url) {
+    try{
+        return fetch(url).then((data) =>  data.json())
+    } catch (error) {
+        console.error(error)
+    }
+}
+
+function loadPokemon() {
+    getAPIData(`https://pokeapi.co/api/v2/pokemon/eevee`).then(
+        (data => console.log(data))
+    )
+}
+
+loadPokemon()
